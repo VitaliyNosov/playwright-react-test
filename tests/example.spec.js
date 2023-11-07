68756328d9f0c0590b1bad2661d0a1e157cdc03d
VitaliyNosov <webdevelorestudio2017@gmail.com>
2023-11-07 16:44:27 +0200

    commit

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -9,11 +9,11 @@ test('test-front-page-one', async ({ page }) => {
 
   await page.goto('https://v2.marketlend.com.au/');
 
-  await page.screenshot({path: 'img/screnshot-front-page.jpg' });
+  await page.screenshot({path: 'screen/screnshot-front-page.jpg' });
   await page.locator('.custom-button-header-one').click();
   await page.locator('#wpforms-242-field_1').fill('vitaliynosov2014@gmail.com');
   const locator = page.locator('.custom-button-header-one');
-  await locator.screenshot({ path: 'img/image.png' });
+  await locator.screenshot({ path: 'screen/image.png' });
   
 });
 
@@ -21,3 +21,4 @@ test('test-front-page-one', async ({ page }) => {
 // test consol comand: npx playwright test
 // test consol comand open brouser: npx playwright test --headed
 // test consol comand code inspect automatic: npx playwright codegen
+// show test: npx playwright show-report
