c9e5ece09277c0e88b1aa81e287e92e9c2da4124
VitaliyNosov <webdevelorestudio2017@gmail.com>
2023-11-08 20:27:04 +0200

    3 commit

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -17,6 +17,44 @@ test('test-front-page-one', async ({ page }) => {
   
 });
 
+// test two
+
+test('test-front-page-two', async ({ page }) => {
+
+  page.setDefaultTimeout(60000);
+
+  await page.goto('https://v2.marketlend.com.au/');
+
+  const locatorTwo = page.locator('.button-front-page-custom-one');
+  await locatorTwo.screenshot({ path: 'img/image-button.png' });
+  await page.getByRole('link', { name: 'Investor' }).click();
+  await page.getByRole('link', { name: 'Borrower' }).click();
+  await page.getByLabel('Primary').getByRole('link', { name: 'Contact Us' }).click();
+  await page.getByRole('link', { name: 'Blog' }).click();
+  await page.getByRole('link', { name: 'About Us' }).click();
+  await page.getByRole('link', { name: 'Partner' }).click();
+  await page.getByLabel('Primary').getByRole('link', { name: 'Privacy' }).click();
+
+});
+
+test('test-contact-page', async ({ page }) => {
+
+  page.setDefaultTimeout(60000);
+
+  await page.goto('https://v2.marketlend.com.au/contact-us/');
+
+  await page.screenshot({path: 'img/screnshot-contact-page.jpg' });
+  await page.getByRole('link', { name: '+610280066798' }).click();
+  await page.getByRole('link', { name: 'Call Us Now' }).click();
+  await page.getByPlaceholder('First Name').fill('Vitaliy');
+  await page.getByPlaceholder('Last Name').fill('Nosov');
+  await page.locator('#form div').filter({ hasText: 'I’m interested in investing' }).nth(4).click();
+  await page.getByPlaceholder('Your Email Address').fill('vitaliynosov2014@gmail.com');
+  await page.getByPlaceholder('Subject').fill('Test works');
+  await page.getByPlaceholder('Your Message').fill('Test text');
+  await page.getByRole('button', { name: 'Request Callback' }).click();
+});
+
 
 // test consol comand: npx playwright test
 // test consol comand open brouser: npx playwright test --headed
